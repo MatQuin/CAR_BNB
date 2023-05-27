06add484cae40505d134c87e7bfba17ac56db162
Matthieu Quiniou <matthieu.quiniou@hotmail.fr>
2023-05-27 17:51:28 +0200

    debut controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,34 @@
 class CarsController < ApplicationController
+
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+
+
+def new
+  @car = Car.new
+end
+
+def create
+  @car = Car.new(params[:car])
+  @car.save
+end
+
+def edit
+end
+
+def update
+  @car.update(params[:car])
+  redirect_to car_path(@car)
+end
+
+def destroy
+  @restaurant.destroy
+end
+
+private
+
+def set_car
+  @car = Car.find(params[:id])
+end
+
 end

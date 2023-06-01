0531871df57cc1610e086bcaa55bc02210f7ef37
Matthieu Quiniou <matthieu.quiniou@hotmail.fr>
2023-06-01 21:05:16 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Car.create!(
+  trademark: 'Volvo',
+  year: '2021',
+  km: '100000',
+  price: 75,
+  user_id: 1
+)
+
+Car.create!(
+  trademark: 'Opel',
+  year: '2022',
+  km: '102000',
+  price: 90,
+  user_id: 1
+)
+
+Car.create!(
+  trademark: 'Audi',
+  year: '2020',
+  km: '1012000',
+  price: 100,
+  user_id: 2
+)
+
+Car.create!(
+  trademark: 'Peugeot',
+  year: '2020',
+  km: '10120020',
+  price: 100,
+  user_id: 2
+)
+
+User.create!(
+  email: 'toto@aoao.com',
+  encrypted_password: '2020',
+  name: 'toto1',
+  surname: 'toto2'
+)
+
+User.create!(
+  email: 'toeazeazeza@aoao.com',
+  encrypted_password: '2020',
+  name: 'touuu',
+  surname: "toazeazea'
+)
